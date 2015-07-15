a9c802bde0690f2561838df5ecc01d5be119812f
Lou, Yufan <loganlyf@gmail.com>
2015-07-15 03:20:35 -0400

    Get react and reimplement hello world in react

---
+++ app/main.jsx
@@ -0,0 +1,17 @@
+import './stylesheets/main.css';
+
+import React from 'react';
+import App from './components/App';
+
+main();
+
+function main() {
+    var app = document.createElement('div');
+    document.body.appendChild(app);
+
+    React.render(<App />, app);
+}
+
+document.body.appendChild(app);
+app.appendChild(component());
+
