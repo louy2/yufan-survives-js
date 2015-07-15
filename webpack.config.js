ecd9a719a20111428d69e77eef049ba6fc24184f
Lou, Yufan <loganlyf@gmail.com>
2015-07-15 01:46:28 -0400

    Reuse common config, utilize env var to tell npm if it's dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 var path = require('path');
+var merge = require('webpack-merge');
 
-module.exports = {
-    entry: path.resolve(__dirname, 'app/main'),
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+    entry: [path.resolve(ROOT_PATH, 'app/main')],
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(ROOT_PATH, 'build'),
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css']
+            }
+        ]
     }
 };
+
+if (TARGET === 'build') {
+    module.exports = common;
+}
+
+if (TARGET === 'dev') {
+    module.exports = common;
+}
