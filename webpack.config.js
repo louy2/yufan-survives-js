e410f491f78c245589ca22eaea458d9070b2608c
Lou, Yufan <loganlyf@gmail.com>
2015-07-15 03:19:14 -0400

    Set up babel loader into webpack
    Sidekick: restrict path traverse of both babel and css

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ var ROOT_PATH = path.resolve(__dirname);
 
 var common = {
     entry: [path.resolve(ROOT_PATH, 'app/main')],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     output: {
         path: path.resolve(ROOT_PATH, 'build'),
         filename: 'bundle.js'
@@ -15,7 +18,13 @@ var common = {
         loaders: [
             {
                 test: /\.css$/,
-                loaders: ['style', 'css']
+                loaders: ['style', 'css'],
+                include: path.resolve(ROOT_PATH, 'app/stylesheets')
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel?stage=1',
+                include: path.resolve(ROOT_PATH, 'app')
             }
         ]
     },
